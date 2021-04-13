b8ac04a974893c61da4552b4155f313cabdd657a
Deniz Ceri <dceri3@gmx.de>
2021-04-14 01:23:43 +0200

    implement range function

--- JsSkill/functions.js
+++ JsSkill/functions.js
@@ -1,1 +1,11 @@
-function range(start,end,ste)
+function range(start,end,step = 1){
+    r = [];
+    for (i = start; i<=end ; i += step){
+        r.push(i);
+
+    }
+    return r
+}
+
+console.log(range(1,10));
+console.log(range(1,10,2));
